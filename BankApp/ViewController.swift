1224b16610668ef355c676d4f9d68e6752976e4f
mac <mac@macbook-pro.local>
2019-02-10 11:53:28 -0200

    Initial Commit

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by mac on 10/02/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
